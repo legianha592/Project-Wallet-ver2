454fb7def4c1c0bf9913b39cf206baf89e6bc4cc
lgnha <lgnha@cmc.com.vn>
2021-09-23 09:38:04 +0700

    add validations

--- src/main/java/org/walletAPI/request/user/ChangePasswordRequest.java
+++ src/main/java/org/walletAPI/request/user/ChangePasswordRequest.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 @RequiredArgsConstructor
@@ -14,9 +15,11 @@ public class ChangePasswordRequest {
 
     @NotBlank(message = "{exception.mandatory.field.is.empty}")
     @Length(min = 6, max = 20, message = "{exception.length.of.password.is.invalid}")
+    @Pattern(regexp = "^[a-zA-Z0-9]", message = "{exception.password.has.invalid.character}")
     private String oldPassword;
 
     @NotBlank(message = "{exception.mandatory.field.is.empty}")
     @Length(min = 6, max = 20, message = "{exception.length.of.password.is.invalid}")
+    @Pattern(regexp = "^[a-zA-Z0-9]", message = "{exception.password.has.invalid.character}")
     private String newPassword;
 }
--- src/main/java/org/walletAPI/request/user/LoginRequest.java
+++ src/main/java/org/walletAPI/request/user/LoginRequest.java
@@ -5,16 +5,19 @@ import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 @RequiredArgsConstructor
 public class LoginRequest {
     @NotBlank(message = "{exception.mandatory.field.is.empty}")
     @Length(min = 6, max = 20, message = "{exception.length.of.userName.is.invalid}")
+    @Pattern(regexp = "^[a-zA-Z0-9]", message = "{exception.userName.has.invalid.character}")
     private String userName;
 
     @NotBlank(message = "{exception.mandatory.field.is.empty}")
     @Length(min = 6, max = 20, message = "{exception.length.of.password.is.invalid}")
+    @Pattern(regexp = "^[a-zA-Z0-9]", message = "{exception.password.has.invalid.character}")
     private String password;
 
     @NotBlank(message = "{exception.mandatory.field.is.empty}")
